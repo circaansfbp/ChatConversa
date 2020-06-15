e5e70e911c22dfea439727b2e51255f41b42ac38
circaansfbp <franciscambp@outlook.com>
2020-06-15 18:51:45 -0400

    ajustes finales

--- app/src/main/java/com/example/chatconversa/Bienvenida_activity.java
+++ app/src/main/java/com/example/chatconversa/Bienvenida_activity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.chatconversa.iniciosesion.InicioSesion;
 import com.example.chatconversa.registrousuarios.RegistroUsuario;
 
 public class Bienvenida_activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
